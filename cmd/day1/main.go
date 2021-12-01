f4bcacdfbfd627ddc5155df437751f7b37b88c6d
Ross Larson <rossl@remitly.com>
2021-12-01 11:29:23 -0800

    Add dummy file for day1

---
+++ cmd/day1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Problem 1: " + problem1())
+	fmt.Println("Problem 2: " + problem2())
+}
+
+func problem1() string {
+	return "Hello D1P1"
+}
+
+func problem2() string {
+	return "Hello D1P2"
+}
