9b3ee3dffc32adc2a2fd141d7768232e7f8b121d
Ross Larson <rossl@remitly.com>
2021-12-01 14:43:04 -0800

    Day 1 Problem 1 + utils

--- cmd/day1/main.go
+++ cmd/day1/main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
+
+	"github.com/relarson/advent-of-code-2021.git/pkg"
 )
 
 func main() {
-	fmt.Println("Problem 1: " + problem1())
+	fmt.Println("Problem 1: " + strconv.Itoa(problem1()))
 	fmt.Println("Problem 2: " + problem2())
 }
 
-func problem1() string {
-	return "Hello D1P1"
+func problem1() int {
+	lines, err := pkg.ReadLinesOfInts("cmd/day1/problem1_input.txt")
+	if err != nil {
+		println(err.Error())
+		return -1
+	}
+
+	previous := math.MaxInt
+	increases := 0
+	for _, depth := range lines {
+		if depth > previous {
+			increases++
+		}
+		previous = depth
+	}
+
+	return increases
 }
 
 func problem2() string {
---
+++ pkg/input_reader.go
@@ -0,0 +1,45 @@
+package pkg
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines convert to ints
+func ReadLinesOfInts(path string) ([]int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		val, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return nil, err
+		}
+		lines = append(lines, val)
+	}
+	return lines, scanner.Err()
+}
