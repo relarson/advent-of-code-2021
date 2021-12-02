300e35db3d0231f67a7921f78c8fb358103807a4
Ross Larson <rossl@remitly.com>
2021-12-02 10:31:10 -0800

    Day 2 Problem 1

--- cmd/day2/main.go
+++ cmd/day2/main.go
@@ -2,17 +2,51 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/relarson/advent-of-code-2021.git/pkg"
 )
 
 func main() {
-	fmt.Println("Problem 1: " + problem1())
-	fmt.Println("Problem 2: " + problem2())
+	fmt.Println("Problem 1: " + strconv.Itoa(problem1()))
+	fmt.Println("Problem 2: " + strconv.Itoa(problem2()))
 }
 
-func problem1() string {
-	return "Hello D2P1"
+func problem1() int {
+	lines, err := pkg.ReadLines("cmd/day2/input.txt")
+	if err != nil {
+		println(err.Error())
+		return -1
+	}
+
+	depth := 0
+	horiz := 0
+
+	for _, line := range lines {
+		components := strings.Split(line, " ")
+		direction := components[0]
+		distance, err := strconv.Atoi(components[1])
+		if err != nil {
+			return -1
+		}
+
+		switch direction {
+		case "up":
+			distance *= -1
+			fallthrough
+		case "down":
+			depth += distance
+		case "forward":
+			horiz += distance
+		default:
+			return -1
+		}
+	}
+
+	return depth * horiz
 }
 
-func problem2() string {
-	return "Hello D2P2"
+func problem2() int {
+	return -1
 }
