d25711832ffb45b0af2089bf9ef9a80faceec81b
Ross Larson <rossl@remitly.com>
2021-12-07 20:58:51 -0800

    Day 6 Problem 1

--- cmd/day6/main.go
+++ cmd/day6/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/relarson/advent-of-code-2021.git/pkg"
 )
@@ -12,14 +13,28 @@ func main() {
 	fmt.Println("Problem 2: " + strconv.Itoa(problem2()))
 }
 
+type Fish struct {
+	startingTimer int
+	lifespan      int
+}
+
 func problem1() int {
-	lines, err := pkg.ReadLines("cmd/day6/test_input.txt")
+	lines, err := pkg.ReadLines("cmd/day6/input.txt")
 	if err != nil {
 		println(err.Error())
 		return -1
 	}
 
-	return len(lines)
+	var toProcess []Fish
+
+	fishStr := strings.Split(lines[0], ",")
+	for _, str := range fishStr {
+		val, _ := strconv.Atoi(str)
+		// original fish all get 80d lifespan
+		toProcess = append(toProcess, Fish{val, 80})
+	}
+
+	return processFish(toProcess)
 }
 
 func problem2() int {
@@ -30,4 +45,29 @@ func problem2() int {
 	}
 
 	return len(lines)
+}
+
+func processFish(toProcess []Fish) int {
+	numberOfFish := len(toProcess)
+
+	for len(toProcess) > 0 {
+		// pop from queue
+		currentFish := toProcess[0]
+		toProcess = toProcess[1:]
+
+		timer := currentFish.startingTimer
+		for currentFish.lifespan > 0 {
+			timeToSpawn := timer + 1
+			currentFish.lifespan -= timeToSpawn
+			if currentFish.lifespan >= 0 {
+				// fish lives to spawn
+				timer = 6 // reset timer
+				// spawn fish with a timer of 8 that has the remaining lifespan
+				toProcess = append(toProcess, Fish{8, currentFish.lifespan})
+				numberOfFish++
+			}
+		}
+	}
+
+	return numberOfFish
 }
