3c474fef42cbc747af0f1c546e37247250dabf55
Ross Larson <rossl@remitly.com>
2021-12-08 21:15:12 -0800

    Day 8 stub

---
+++ cmd/day8/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/relarson/advent-of-code-2021.git/pkg"
+)
+
+func main() {
+	fmt.Println("Problem 1: " + strconv.Itoa(problem1()))
+	fmt.Println("Problem 2: " + strconv.Itoa(problem2()))
+}
+
+func problem1() int {
+	lines, err := pkg.ReadLines("cmd/day8/input.txt")
+	if err != nil {
+		println(err.Error())
+		return -1
+	}
+
+	return len(lines)
+}
+
+func problem2() int {
+	lines, err := pkg.ReadLines("cmd/day8/input.txt")
+	if err != nil {
+		println(err.Error())
+		return -1
+	}
+
+	return len(lines)
+}
