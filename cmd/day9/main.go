c30fbc5b3d90120f52d90cfddecf3cf026aeaf32
Ross Larson <rossl@remitly.com>
2021-12-09 22:00:07 -0800

    Day 9 Problem 2

--- cmd/day9/main.go
+++ cmd/day9/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 
 	"github.com/relarson/advent-of-code-2021.git/pkg"
@@ -36,13 +37,24 @@ func problem1() int {
 }
 
 func problem2() int {
-	matrix, err := pkg.ReadIntMatrix("cmd/day9/test_input.txt", "")
+	matrix, err := pkg.ReadIntMatrix("cmd/day9/input.txt", "")
 	if err != nil {
 		println(err.Error())
 		return -1
 	}
 
-	return len(matrix)
+	lowPoints := findLowPoints(matrix)
+
+	var sizes []int
+	for _, point := range lowPoints {
+		basinSize := findBasinSize(point, matrix)
+		sizes = append(sizes, basinSize)
+	}
+	sort.Ints(sizes)
+
+	lastIndex := len(sizes) - 1
+
+	return sizes[lastIndex] * sizes[lastIndex-1] * sizes[lastIndex-2]
 }
 
 func findLowPoints(matrix [][]int) []Point {
@@ -77,4 +89,56 @@ func findLowPoints(matrix [][]int) []Point {
 		}
 	}
 	return lowPoints
+}
+
+func findBasinSize(basinOrigin Point, matrix [][]int) int {
+	settled := make(map[Point]bool, 0)
+	var queue []Point
+
+	queue = append(queue, basinOrigin)
+
+	for len(queue) > 0 {
+		cp := queue[0]
+		queue = queue[1:]
+		cv := matrix[cp.row][cp.column]
+		neighbors := buildNeighbors(cp, matrix)
+		for _, n := range neighbors {
+			_, ok := settled[n]
+			if ok {
+				continue
+			}
+			nv := matrix[n.row][n.column]
+			if nv > cv && nv < 9 {
+				queue = append(queue, n)
+			}
+		}
+		settled[cp] = true
+	}
+
+	return len(settled)
+}
+
+func buildNeighbors(pt Point, matrix [][]int) []Point {
+	var neighbors []Point
+
+	r := pt.row
+	c := pt.column
+
+	if r > 0 {
+		neighbors = append(neighbors, Point{r - 1, c})
+	}
+
+	if r < (len(matrix) - 1) {
+		neighbors = append(neighbors, Point{r + 1, c})
+	}
+
+	if c > 0 {
+		neighbors = append(neighbors, Point{r, c - 1})
+	}
+
+	if c < (len(matrix[0]) - 1) {
+		neighbors = append(neighbors, Point{r, c + 1})
+	}
+
+	return neighbors
 }
